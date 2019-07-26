ec59609a912d1ce0c17ae03e08ced645d328fe1c
Michael Johnson <youngmug@animeneko.net>
2019-07-26 09:07:07 -0700

    Clean Up Download List
    Make the download list group by OS. Add header indicating target OS. Rewrite the text guide to only highlight the OS and the preferred format for each OS.

--- src/app/download-box/download-box.component.ts
+++ src/app/download-box/download-box.component.ts
@@ -1,31 +1,53 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 interface ReleaseAsset {
   browser_download_url: string;
   name: string;
   download_count: number;
 }
 @Component({
-  selector: 'app-download-box',
-  templateUrl: './download-box.component.html',
-  styleUrls: ['./download-box.component.css']
+  selector: "app-download-box",
+  templateUrl: "./download-box.component.html",
+  styleUrls: ["./download-box.component.css"]
 })
 export class DownloadBoxComponent implements OnInit {
   assets: ReleaseAsset[];
-  constructor() { }
+  assetsWindows: ReleaseAsset[];
+  assetsMac: ReleaseAsset[];
+  assetsLinux: ReleaseAsset[];
+  constructor() {}
 
   ngOnInit() {
-    fetch('https://api.github.com/repos/the-expanse/SideQuest/releases/latest')
+    fetch("https://api.github.com/repos/the-expanse/SideQuest/releases/latest")
       .then(r => r.json())
       .then(r => {
         r.assets.reverse();
         this.assets = r.assets.filter(a => {
-          console.log(a.name.split('.').pop());
-          return ~['zip', 'exe', 'xz', 'dmg'].indexOf(a.name.split('.').pop());
+          console.log(a.name.split(".").pop());
+          return ~["zip", "exe", "xz", "dmg"].indexOf(a.name.split(".").pop());
+        });
+        this.assetsWindows = r.assets.filter(a => {
+          if (a.name.search(/\.exe$/) != -1) {
+            return a;
+          } else if (a.name.search(/win\.zip$/) != -1) {
+            return a;
+          }
+        });
+        this.assetsMac = r.assets.filter(a => {
+          if (a.name.search(/\.dmg$/) != -1) {
+            return a;
+          } else if (a.name.search(/mac\.zip$/) != -1) {
+            return a;
+          }
+        });
+        this.assetsLinux = r.assets.filter(a => {
+          if (a.name.search(/\.tar\...$/) != -1) {
+            return a;
+          }
         });
       });
   }
 
-  openUrl(url: string){
+  openUrl(url: string) {
     window.open(url);
   }
 }
