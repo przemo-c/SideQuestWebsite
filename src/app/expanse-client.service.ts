eb79d8dae8a52edda4ca42dedad7865d6b07e72c
Shane Harris <shane@theexpanse.app>
2020-02-22 12:31:44 +0000

    moved back to the production servers

--- src/app/expanse-client.service.ts
+++ src/app/expanse-client.service.ts
@@ -33,11 +33,11 @@ export class ExpanseClientService {
 
     this.url = !!this.isDev
       ? "ws://192.168.0.34:3000"
-      : "wss://api.theexpanse.app";
+      : "wss://api.sidequestvr.com";
 
     this.cdnUrl = this.isDev
       ? "http://192.168.0.4:47000/"
-      : "https://cdn.theexpanse.app/";
+      : "https://cdn.sidequestvr.com/";
 
     this.shortenerUrl = this.isDev
       ? "http://192.168.0.4:47499/"
