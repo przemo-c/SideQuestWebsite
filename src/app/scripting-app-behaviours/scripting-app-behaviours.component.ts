13e21bc0fdc67cb7d59731a18d11351f2ac651b1
Shane Harris <shane@theexpanse.app>
2020-03-07 10:34:21 +0000

    updated documentation for interactables in the legends sdk

--- src/app/scripting-app-behaviours/scripting-app-behaviours.component.ts
+++ src/app/scripting-app-behaviours/scripting-app-behaviours.component.ts
@@ -1,56 +1,71 @@
-import { Component, OnInit } from "@angular/core";
-import { AppService } from "../app.service";
+import { Component, OnInit } from '@angular/core';
+import { AppService } from '../app.service';
 
 @Component({
-  selector: "app-scripting-app-behaviours",
-  templateUrl: "./scripting-app-behaviours.component.html",
-  styleUrls: ["./scripting-app-behaviours.component.css"]
+    selector: 'app-scripting-app-behaviours',
+    templateUrl: './scripting-app-behaviours.component.html',
+    styleUrls: ['./scripting-app-behaviours.component.css'],
 })
 export class ScriptingAppBehavioursComponent implements OnInit {
-  appBehaviourSource = `using UnityEngine;
-public class AppBehaviour : MonoBehaviour {}`;
-
-  appBehaviour = `// MyAppScript.cs
-using UnityEngine;
-public class MyAppScript : AppBehaviour {
-  string MyAppVersion = "0.0.1";
-  // Use this for initialization
-  void Start () {
-    
-  }
-  // Update is called once per frame
-  void Update () {
-    
+    appBehaviourSource = `using UnityEngine;
+public class AppBehaviour : MonoBehaviour {
+  public virtual void Start(){
+    // Initialisation is done here...
   }
 }`;
-  appBehaviourAddComponent = `// MyAppScript.cs
+
+    appBehaviour = `// MyAppScript.cs
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyAppScript : AppBehaviour
+{
+  // Start is called before the first frame update.
+  public override void Start() {
+
+    // Your code here...
+
+    // Call base.Start() to initialise the underlying AppBehaviour. Without this
+    // some things might not work as expected like interactables.
+    base.Start();
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+
+  }
+}`;
+    appBehaviourAddComponent = `// MyAppScript.cs
 using UnityEngine;
 public class MyAppScript : AppBehaviour {
   // Use this for initialization
-  void Start () {
+  public override void Start() {
     MyOtherScript component = transform.Find("ChildGameObject/AnotherChild").gameObject.AddComponent<MyOtherScript>();
     component.rootTransform = transform;
+    base.Start();
   }
   // Update is called once per frame
   void Update () {
-    
+
   }
 }`;
-  appBehaviourAddComponentOther = `// MyOtherScript.cs
+    appBehaviourAddComponentOther = `// MyOtherScript.cs
 using UnityEngine;
 public class MyOtherScript : MonoBehaviour {
   public Transform rootTransform;
   // Use this for initialization
   void Start () {
-    Debug.Log(rootTransform.position); 
+    Debug.Log(rootTransform.position);
     // Logs the position of the root of your app
   }
   // Update is called once per frame
   void Update () {
-    
+
   }
 }`;
-  constructor(public appService: AppService) {}
+    constructor(public appService: AppService) {}
 
-  ngOnInit() {}
+    ngOnInit() {}
 }
--- src/app/scripting-interaction/scripting-interaction.component.ts
+++ src/app/scripting-interaction/scripting-interaction.component.ts
@@ -1,84 +1,40 @@
-import { Component, OnInit } from "@angular/core";
-import { AppService } from "../app.service";
+import { Component, OnInit } from '@angular/core';
+import { AppService } from '../app.service';
 
 @Component({
-  selector: "app-scripting-interaction",
-  templateUrl: "./scripting-interaction.component.html",
-  styleUrls: ["./scripting-interaction.component.css"]
+    selector: 'app-scripting-interaction',
+    templateUrl: './scripting-interaction.component.html',
+    styleUrls: ['./scripting-interaction.component.css'],
 })
 export class ScriptingInteractionComponent implements OnInit {
-  interactionCode = ` // MyScript.cs
+    interactionEventCode = ` // MyScript.cs
 using UnityEngine;
 using SideQuestSDK;
 public class MyScript : MonoBehaviour {
   // Use this for initialization
   void Start () {
-    API.SetupInteraction(gameObject,false/*isClickable*/, false/*isTeleport*/, true/*isGrabable*/);
-
-    // If you need to destroy this interactable later then instead of calling Destroy(gameObject)
-    // use API.DestroyInteractable(gameObject) which will also remove it from the network sync slot etc.
-    // If you want to remove the interactable from the hand but not destroy it use API.DropInteractable(gameObject)
-    // and you can call API.DestroyInteractable(gameObject) at a later stage to remove it entirely.
-  }
-
-  // Update is called once per frame
-  void Update () {
-
-  }
-  // Left land grabbed duh
-  void OnGrabbedLeft (Transform transform) {
-
-  }
-  // ..
-  void OnDroppedLeft (Transform transform) {
-
-  }
-  // ..
-  void OnGrabbedRight (Transform transform) {
-
-  }
-  // ..
-  void OnDroppedRight (Transform transform) {
-
+    var interactable = GetComponent<Interactable>();
+    interactable.onSelectEnter.AddListener((interactor) => {
+       // Do something when the object is picked up.
+    });
+    interactable.onSelectExit.AddListener((interactor) => {
+       // Do something when the object is dropped.
+    });
+    interactable.onActivate.AddListener((interactor) => {
+       // Do something when the object is clicked ( trigger pressed ).
+    });
+    interactable.onDeactivate.AddListener((interactor) => {
+       // Do something when the object is unclicked ( trigger released ).
+    });
+    interactable.onHoverEnter.AddListener((interactor) => {
+       // Do something when the object becomes selectable.
+    });
+    interactable.onHoverExit.AddListener((interactor) => {
+       // Do something when the object is no longer selectable.
+    });
   }
 }`;
+    constructor(public appService: AppService) {}
 
-  interactionCodeAlt = ` // MyScript.cs
-using UnityEngine;
-using SideQuestSDK;
-public class MyScript : MonoBehaviour {
-  // Use this for initialization
-  void Start () {
-    var settings = gameObject.AddComponent<InteractionSettings>();
-    settings.isClickable = true;
-    settings.isTeleport = true;
-    settings.isGrabable = true;
-
-    /*
-      public bool isClickable = false;
-      public bool isTeleport = false;
-      public bool isGrabable = false;
-      public bool isResizable = false;
-      public bool isTelekinesis = false;
-      public bool isPhysics = false;
-      public bool physicsGravity = true;
-      public float physicsMass = 1;
-      public float physicsDrag = 0;
-      public float physicsAngularDrag = 0.005f;
-      public bool isSpawner = false;
-      public float reSpawn = 50f;
-      public bool isFixedGrab = false;
-
-
-      public Vector3 primaryGrabPosition = Vector3.zero;
-      public Vector3 primaryGrabRotation = Vector3.zero;
-      // TODO: two hand grab coming soon
-      public bool isTwoHandFixedGrab = false;
-      public Vector3 secondaryGrabPosition = Vector3.zero;
-    */
-  }
-}`;
-  constructor(public appService: AppService) {}
-
-  ngOnInit() {}
+    ngOnInit() {}
 }
